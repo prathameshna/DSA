78180f9484643730546ecf34c6e4bca38206ece1
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-07-02 07:06:03 +0530

    first commit

---
+++ sumOfNatNumber.cpp
@@ -0,0 +1,23 @@
+// Q sum of first natural number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int n)
+{
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        ans +=i;
+
+        return ans;
+    }
+}
+int32_t main()
+{
+
+    int n;
+    cin >> n;
+
+    cout << sum(n) << endl;
+}
