4e494254408a6bd6f9bf55f0cc66fe7ee9cc0d5b
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-07-04 07:48:18 +0530

    first commit

---
+++ MaxMinArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+#include <climits>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    int MaxNo;
+    int MinNo;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] > MaxNo)
+        {
+            MaxNo = array[i];
+        }
+        if (array[i] < MinNo)
+        {
+            MinNo = array[i];
+        }
+    }
+    return 0;
+}
---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int array[4] = {12, 13, 14, 15};
+    cout << array[3] << endl;
+
+    return 0;
+}
